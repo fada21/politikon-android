43f535c920a5e713c2af709aa0552b47c13ac378
fada21 <lukasz.czaplicki@gmail.com>
2016-06-27 02:15:49 +0100

    add dummy resorces

--- app/src/main/java/com/fada21/android/politikon/HomeActivity.java
+++ app/src/main/java/com/fada21/android/politikon/HomeActivity.java
@@ -30,7 +30,7 @@ public class HomeActivity extends AppCompatActivity
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    Snackbar.make(view, "Quick pick - not implemented yet", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
             });
