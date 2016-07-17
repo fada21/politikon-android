a6bb873c54e7ba485fba936a416c6235f7bbfb4f
fada21 <lukasz.czaplicki@gmail.com>
2016-07-18 00:55:53 +0100

    add Bet, modify gitignore -> exclude whole .idea

--- app/src/main/java/com/fada21/android/politikon/Bet.java
+++ app/src/main/java/com/fada21/android/politikon/Bet.java
@@ -1,5 +1,16 @@
 package com.fada21.android.politikon;
 
+import java.util.List;
+
 public class Bet {
+
+    public String title;
+    public String desc;
+    public List<String> tags;
+    public int yes_price;
+    public int no_price;
+
+    public long added_date;
+    public long due_date;
 
 }
