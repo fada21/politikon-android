3219699b4349e520dc7296c911c3fb3350ed6b23
fada21 <lukasz.czaplicki@gmail.com>
2016-11-15 23:52:06 +0000

    add models for network repo

---
+++ app/src/main/java/com/fada21/android/politikon/models/Event.java
@@ -0,0 +1,50 @@
+package com.fada21.android.politikon.models;
+
+public class Event {
+    String title;
+    String short_title;
+    String twitter_tag;
+    String title_fb_yes;
+    String title_fb_no;
+    String description;
+    String small_image;
+    String big_image;
+    boolean is_feaured;
+    int outcome;
+    String outcome_reason;
+
+
+//    {
+//        "count": 360,
+//            "next": "https://www.politikon.org.pl/api/events/?page=2&page_size=5",
+//            "previous": null,
+//            "results": [
+//        {
+//            "id": 236,
+//                "title": "Tylko polskie piosenki w TOP10 \"Trójki\"",
+//                "short_title": "Tylko polskie piosenki w TOP10 \"Trójki\"",
+//                "twitter_tag": "lp3",
+//                "title_fb_yes": "Tylko polskie piosenki w TOP10 \"Trójki\"",
+//                "title_fb_no": "Tylko polskie piosenki w TOP10 \"Trójki\"",
+//                "description": "W pierwszej dziesiątce ostatniego sierpniowego notowania Listy Przebojów (nr 1804) radiowej \"Trójki\" będą wyłącznie utwory polskich wykonawców.\r\n\r\nTu aktualne notowanie: http://lp3.polskieradio.pl/",
+//                "small_image": "https://politik.s3.amazonaws.com/events_small/przes%C5%82ano_27.5.2016_o_14_30_56.png",
+//                "big_image": "https://politik.s3.amazonaws.com/events_big/przes%C5%82ano_27.5.2016_o_14_30_56.png",
+//                "is_featured": true,
+//                "outcome": 4,
+//                "outcome_reason": "Piosenka zespołu Metallica na 4. i Joe Bonamassy na 10. miejscu w 1804. notowaniu rozstrzyga zakład na \"NIE\"",
+//                "created_date": "2016-08-16T18:43:11.295055Z",
+//                "created_by": 39,
+//                "estimated_end_date": "2016-08-26T18:40:57Z",
+//                "end_date": "2016-08-28T12:21:51.255641Z",
+//                "current_buy_for_price": 20,
+//                "current_buy_against_price": 80,
+//                "current_sell_against_price": 77,
+//                "last_transaction_date": null,
+//                "Q_for": 6,
+//                "Q_against": 13,
+//                "turnover": 19,
+//                "absolute_price_change": 7,
+//                "price_change": -7,
+//                "B": 5.0
+//        },
+}
