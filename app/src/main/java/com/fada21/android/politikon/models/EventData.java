89a92a50c4d03229cf8c0ea7c6f6a886669cfa4d
fada21 <lukasz.czaplicki@gmail.com>
2016-11-16 01:04:02 +0000

    add wrapper model for events

---
+++ app/src/main/java/com/fada21/android/politikon/models/EventData.java
@@ -0,0 +1,14 @@
+package com.fada21.android.politikon.models;
+
+import java.util.List;
+
+public class EventData {
+    int count;
+    List<Event> results;
+
+    //    {
+//        "count": 360,
+//            "next": "https://www.politikon.org.pl/api/events/?page=2&page_size=5",
+//            "previous": null,
+//            "results":
+}
